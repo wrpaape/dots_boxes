7b4f47a7fd00fdb44e3d1ad060881dad5e53454c
Reid Paape <wrpaape@gmail.com>
2015-08-26 17:23:05 -0500

    set up assign_children method in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,21 @@
-def self.assign_children
-  parentModel = keys.first.constantize
-  children = delete(:children)
-  parent = parentModel.create(values.first)
-  return unless children
-  children.each do |key, value|
-    child = value.assign_children
-    foreign_key = child.column_names.grep(/_id/).first
-    child.update({ foreign_key=> parent.id })
+class Hash
+  def assign_children
+    model = keys.first.constantize
+    attributes = values.first
+    children = attributes.delete(:children) || []
+    parent = model.create(attributes)
+    children.each do |child_hash|
+      child = child_hash.assign_children
+      foreign_key = child.class.column_names.grep(/_id/).first
+      child.update({ foreign_key => parent.id })
+    end
+    parent
   end
 end
 
 games = [
   {
-    "Game"=> {
+    "Game" => {
       title: "Dots and Boxes",
       rules:
   """
@@ -22,37 +25,49 @@ games = [
   The winner of the game is the player with the most points.
   """[1..-1].gsub!("\n", "  "),
       component: "DotsBoxes",
-      children: {
-        "Spec"=> {
-          min: 2,
-          max: 20,
-          children: {
-            "Player"=> {
-              min: 0,
-              max: 10
-            },
-            "Computer"=> {
-              min: 0,
-              max: 10,
-              easy: true,
-              medium: true,
-              hard: true
-            },
-            "Grid"=> {
-              children: {
-                "Row"=> {
-                  min: 1,
+      children: [
+        {
+          "Spec" => {
+            min: 2,
+            max: 20,
+            children: [
+              {
+                "Player" => {
+                  min: 0,
                   max: 10
-                },
-                "Col"=> {
-                  min: 1,
-                  max: 10
-                },
+                }
+              },
+              {
+                "Computer" => {
+                  min: 0,
+                  max: 10,
+                  easy: true,
+                  medium: true,
+                  hard: true
+                }
+              },
+              {
+                "Grid" => {
+                  children: [
+                    {
+                      "Row" => {
+                        min: 1,
+                        max: 10
+                      }
+                    },
+                    {
+                      "Col" => {
+                        min: 1,
+                        max: 10
+                      },
+                    }
+                  ]
+                }
               }
-            }
+            ]
           }
         }
-      }
+      ]
     }
   }
 ]
